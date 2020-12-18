cc5f723d192e813b23e94ffd6f9ef09f3e5e50cd
Matthew Zenittini <matt@playstowin.com>
2020-12-18 16:21:11 -0500

    setup webpack + website structure

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    module: {
+        rules: [
+        {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        }
+        ]
+    },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+    })
+    ]
+};
