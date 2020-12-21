67bb7975e84c5782d0def158a1f618db6daf38c7
Matthew Zenittini <matt@playstowin.com>
2020-12-20 22:38:19 -0500

    completed lots of design - implemented copy-webpack-plugin for image loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require("path");
 
 module.exports = {
@@ -7,12 +8,20 @@ module.exports = {
         {
             test: /\.scss$/,
             use: ["style-loader", "css-loader", "sass-loader"]
-        }
+        },
+        {
+            test: /\.(jpg|png)$/,
+            use: {loader: 'url-loader'}
+        },
         ]
     },
     plugins: [
-    new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", "index.html")
-    })
-    ]
-};
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html")}),
+    new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "src/images",
+            to: "./images",
+          },
+        ]
+    })]}
