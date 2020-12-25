06eba5648fe01cee7805dea866e567112aac74ce
Matthew Zenittini <matt@playstowin.com>
2020-12-24 23:51:52 -0500

    finished responsive + latest dist build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.scss";
+
+const toggle = document.getElementsByClassName('header__nav__toggle')[0]
+const links = document.getElementsByClassName('header__nav__links')[0]
+
+toggle.addEventListener('click', () => {
+    links.classList.toggle('active')
+} )
